425ae0e3318dbba5977871535e2e22939e1344a8
chaoide <geunkilchoi@gmail.com>
2018-01-11 08:37:44 +0900

    Going Mobile

--- scripts/jewel.js
+++ scripts/jewel.js
@@ -59,20 +59,49 @@ var jewel = (function() {
             $ = dom.$,
             activeScreen = $("#game .screen.active")[0],
             screen = $("#" + screenId)[0];
+        if (!jewel.screens[screenId]) {
+            alert("This module is not implemented yet!");
+            return;
+        }
         if (activeScreen) {
             dom.removeClass(activeScreen, "active");
         }
         dom.addClass(screen, "active");
+        // run the screen module
+        jewel.screens[screenId].run();
     }
 
-    
+    function isStandalone() {
+        return (window.navigator.standalone !== false);
+    }
+
     function setup() {
-        jewel.showScreen("splash-screen");
+        // hide the address bar on Android devices
+        if (/Android/.test(navigator.userAgent)) {
+            jewel.dom.$("html")[0].style.height = "200%";
+            setTimeout(function() {
+                window.scrollTo(0, 1);
+            }, 0);
+        }
+
+        // disable native touchmove behavior to 
+        // prevent overscroll
+        jewel.dom.bind(document, "touchmove", function(event) {
+            event.preventDefault();
+        });
+
+        if (isStandalone()) {
+            showScreen("splash-screen");
+        } else {
+            showScreen("install-screen");
+        }
     }
 
     return {
+        isStandalone: isStandalone,
         load: load,
         setup: setup,
-        showScreen : showScreen
+        showScreen : showScreen,
+        screens: {}
     };
 })();
